fa366cbb2d7ba2cbc284e13f43440e0d457e9916
Leonardo <leosouza5@hotmail.com.br>
2024-04-25 20:17:18 -0400

    first commit

---
+++ src/main/java/br/unigran/aulaandre/AulaAndre.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.unigran.aulaandre;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class AulaAndre {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
